35d2000e34eba0bbb2905f0896f5efa7172416fd
lkkchung <33067123+lkkchung@users.noreply.github.com>
2017-10-25 03:19:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -63,34 +63,6 @@ function draw() {
     textSize(50);
     text("FINISHED")
   }
-
-  // if (play == false){
-  //   fill(255, 0, 0);
-  //   textAlign(LEFT);
-  //   textSize(maxDiam / 2);
-  //   for (let i = 0; i < numeros.length; i++){
-  //     text(numeros[i], newTextX + i * maxDiam, 250);
-  //   }
-  //   newTextX -= 5;
-  // }
-
-
-  // if (play == true) {
-  //   for (let i = 0; i < width / maxDiam; i ++) {
-  //     for (let j = 0; j < height / maxDiam; j ++) {
-  //       pixy[i][j] = randomRadius();
-  //       fill(255);
-  //       ellipse(i * maxDiam + maxDiam / 2, j * maxDiam + maxDiam / 2, pixy[i][j], pixy[i][j]);
-  //     }
-  //   }
-  // } else {
-  //   for (let i = 0; i < width / maxDiam; i ++) {
-  //     for (let j = 0; j < height / maxDiam; j ++) {
-  //       fill(255);
-  //       ellipse(i * maxDiam + maxDiam / 2, j * maxDiam + maxDiam / 2, pixy[i][j], pixy[i][j]);
-  //     }
-  //   }
-  // }
 }
 
 function resetSketch() {
@@ -140,17 +112,6 @@ function pauseSketch() {
 function goSketch() {
   go = true;
 }
-
-// function makePixels(img, x, y) {
-//   let imgPixel = img;
-//   let pixelX = x;
-//   let pixelY = y;
-//   let a = 0;
-//
-// 	a = img.get(pixelX * maxDiam, pixelY * maxDiam);
-//   return int(brightness(a));
-// }
-
 
 function serialEvent(){
   let inString = serial.readStringUntil('\r\n');
@@ -222,74 +183,3 @@ function serialError(err) {
 function portClose() {
   print('The serial port closed.');
 }
-
-// noprotect
-
-// var serial;          // variable to hold an instance of the serialport library
-// var portName = '/dev/cu.usbmodem1411'; // fill in your serial port name here
-//
-// function setup() {
-//   createCanvas(640, 480);          // make canvas
-//   smooth();                        // antialias drawing lines
-//   serial = new p5.SerialPort();    // make a new instance of the serialport library
-//   serial.on('list', printList);    // set a callback function for the serialport list event
-//   serial.on('connected', serverConnected); // callback for connecting to the server
-//   serial.on('open', portOpen);     // callback for the port opening
-//   serial.on('data', serialEvent);  // callback for when new data arrives
-//   serial.on('error', serialError); // callback for errors
-//   serial.on('close', portClose);   // callback for the port closing
-//
-//   serial.list();                   // list the serial ports
-//   serial.open(portName);           // open a serial port
-// }
-//
-// var locH, locV;        // location of the circle
-// var circleColor = 255; // color of the circle
-//
-// function draw() {
-//   background(0);               // black background
-//   fill(circleColor);           // fill depends on the button
-//   ellipse(locH, locV, 50, 50); // draw the circle
-// }
-//
-// // get the list of ports:
-// function printList(portList) {
-//   // portList is an array of serial port names
-//   for (var i = 0; i < portList.length; i++) {
-//   // Display the list the console:
-//   print(i + " " + portList[i]);
-//   }
-// }
-//
-// function serverConnected() {
-//   print('connected to server.');
-// }
-//
-// function portOpen() {
-//   print('the serial port opened.')
-// }
-//
-// function serialError(err) {
-//   print('Something went wrong with the serial port. ' + err);
-// }
-//
-// function portClose() {
-//   print('The serial port closed.');
-// }
-//
-// function serialEvent() {
-//   // read a string from the serial port
-//   // until you get carriage return and newline:
-//   var inString = serial.readStringUntil('\r\n');
-//
-//   //check to see that there's actually a string there:
-//   if (inString.length > 0 ) {
-//     var sensors = split(inString, ',');            // split the string on the commas
-//     if (sensors.length > 2) {                      // if there are three elements
-//       locH = map(sensors[0], 0, 1023, 0,width);   // element 0 is the locH
-//       locV = map(sensors[1], 0, 1023, 0, height); // element 1 is the locV
-//       circleColor = 255 - (sensors[2] * 255);      // element 2 is the button
-//     }
-//   }
-// }
-//
